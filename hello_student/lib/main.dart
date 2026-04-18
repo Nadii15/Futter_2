41f18c0cfe2248c99448eb1418adabfd36295eeb
okolodk <grisha.zhivot@bk.ru>
2026-04-18 15:45:26 +0300

    completed task 2

---
+++ hello_student/lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'Привет! Меня зовут Надежда.',
+                style: TextStyle(fontSize: 24),
+              ),
+              Text(
+                'Я студент группы ИСП-233.',
+                style: TextStyle(fontSize: 24),
+              ),
+            ],
+          ),
+        ),
+      ),
+    ),
+  );
+}
